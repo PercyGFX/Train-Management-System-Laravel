6a4255d8b612697d2b59179988658b4a9a55487a
PercyGFX <isurangabtk@gmail.com>
2023-07-25 19:23:44 +0530

    Update layout.blade.php

--- train-ticket/resources/views/layout.blade.php
+++ train-ticket/resources/views/layout.blade.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Destino</title>
+<title>About us</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Destino project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="frontend/styles/bootstrap4/bootstrap.min.css">
 <link href="frontend/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="frontend/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
-<link rel="stylesheet" type="text/css" href="frontend/plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
-<link rel="stylesheet" type="text/css" href="frontend/plugins/OwlCarousel2-2.2.1/animate.css">
-<link href="frontend/plugins/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="frontend/styles/main_styles.css">
-<link rel="stylesheet" type="text/css" href="frontend/styles/responsive.css">
+<link rel="stylesheet" type="text/css" href="frontend/styles/about_styles.css">
+<link rel="stylesheet" type="text/css" href="frontend/styles/about_responsive.css">
 </head>
 <body>
 
 <div class="super_container">
-
+	
 	<!-- Header -->
 
 	<header class="header">
@@ -30,7 +26,7 @@
 						<!-- Logo -->
 						<div class="logo_container">
 							<div class="logo">
-								<div>Train Management</div>
+								<div>destino</div>
 								<div>travel agency</div>
 								<div class="logo_image"><img src="frontend/images/logo.png" alt=""></div>
 							</div>
@@ -39,8 +35,8 @@
 						<!-- Main Navigation -->
 						<nav class="main_nav ml-auto">
 							<ul class="main_nav_list">
-								<li class="main_nav_item active"><a href="#">Home</a></li>
-								<li class="main_nav_item"><a href="about.html">About us</a></li>
+								<li class="main_nav_item"><a href="index.html">Home</a></li>
+								<li class="main_nav_item active"><a href="#">About us</a></li>
 								<li class="main_nav_item"><a href="offers.html">Offers</a></li>
 								<li class="main_nav_item"><a href="news.html">News</a></li>
 								<li class="main_nav_item"><a href="contact.html">Contact</a></li>
@@ -83,15 +79,15 @@
 					</form>
 				</div>
 				<ul class="menu_list menu_mm">
-					<li class="menu_item menu_mm"><a href="#">Home</a></li>
-					<li class="menu_item menu_mm"><a href="about.html">About us</a></li>
+					<li class="menu_item menu_mm"><a href="index.html">Home</a></li>
+					<li class="menu_item menu_mm"><a href="#">About us</a></li>
 					<li class="menu_item menu_mm"><a href="offers.html">Offers</a></li>
 					<li class="menu_item menu_mm"><a href="news.html">News</a></li>
 					<li class="menu_item menu_mm"><a href="contact.html">Contact</a></li>
 				</ul>
 
 				<!-- Menu Social -->
-
+				
 				<div class="menu_social_container menu_mm">
 					<ul class="menu_social menu_mm">
 						<li class="menu_social_item menu_mm"><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
@@ -102,64 +98,81 @@
 					</ul>
 				</div>
 
-				
+				<div class="menu_copyright menu_mm">Colorlib All rights reserved</div>
 			</div>
 
 		</div>
 
 	</div>
-
+	
 	<!-- Home -->
 
 	<div class="home">
-		<div class="home_background" style="background-image:url(frontend/images/home.jpg)"></div>
-		<div class="home_content">
-			<div class="home_content_inner">
-				<div class="home_text_large">discover</div>
-				<div class="home_text_small">Efficient train service</div>
+		<!-- Image by  -->
+		<div class="home_background parallax-window" data-parallax="scroll" data-image-src="frontend/images/about_background.jpg" data-speed="0.8"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="home_content">
+						<div class="home_content_inner">
+							<div class="home_title">About us</div>
+							<div class="home_breadcrumbs">
+								<ul class="home_breadcrumbs_list">
+									<li class="home_breadcrumb"><a href="index.html">Home</a></li>
+									<li class="home_breadcrumb">About us</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
+		</div>		
 	</div>
 
 	<!-- Find Form -->
 
 	<div class="find">
 		<!-- Image by https://unsplash.com/@garciasaldana_ -->
-		<div class="find_background parallax-window" data-parallax="scroll" data-image-src="frontend/images/find.jpg" data-speed="0.8"></div>
+		<div class="find_background_container prlx_parent">
+			<div class="find_background prlx" style="background-image:url(frontend/images/find.jpg)"></div>
+		</div>
+		<!-- <div class="find_background parallax-window" data-parallax="scroll" data-image-src="images/find.jpg" data-speed="0.8"></div> -->
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<div class="find_title text-center">Find your train to your destination</div>
+					<div class="find_title text-center">Find the Adventure of a lifetime</div>
 				</div>
 				<div class="col-12">
 					<div class="find_form_container">
 						<form action="#" id="find_form" class="find_form d-flex flex-md-row flex-column align-items-md-center align-items-start justify-content-md-between justify-content-start flex-wrap">
-
 							<div class="find_item">
-								<div>From Location</div>
+								<div>Destination:</div>
+								<input type="text" class="destination find_input" required="required" placeholder="Keyword here">
+							</div>
+							<div class="find_item">
+								<div>Adventure type:</div>
+								<select name="adventure" id="adventure" class="dropdown_item_select find_input">
+									<option>Categories</option>
+									<option>Categories</option>
+									<option>Categories</option>
+								</select>
+							</div>
+							<div class="find_item">
+								<div>Min price</div>
 								<select name="min_price" id="min_price" class="dropdown_item_select find_input">
 									<option>&nbsp;</option>
-									<option>Colombo</option>
-									<option>Matara</option>
+									<option>Price</option>
+									<option>Price</option>
 								</select>
 							</div>
 							<div class="find_item">
-								<div>To Location</div>
+								<div>Max price</div>
 								<select name="max_price" id="max_price" class="dropdown_item_select find_input">
 									<option>&nbsp;</option>
-									<option>Kandy</option>
-									<option>Trinco</option>
+									<option>Price</option>
+									<option>Price</option>
 								</select>
 							</div>
-
-							<div class="find_item">
-                            		<div>Date</div>
-                            		<select name="max_price" id="max_price" class="dropdown_item_select find_input">
-                            			<option>&nbsp;</option>
-                            		    <option>Kandy</option>
-                            			<option>Trinco</option>
-                            		</select>
-                            		</div>
 							<button class="button find_button">Find</button>
 						</form>
 					</div>
@@ -168,73 +181,90 @@
 		</div>
 	</div>
 
-	<!-- Top Destinations -->
+	<!-- About -->
 
-	<div class="top">
+	<div class="about">
 		<div class="container">
 			<div class="row">
 				<div class="col">
 					<div class="section_title text-center">
-						<h2>Top trains in sri lanka</h2>
-						<div>Find best train matches to you</div>
+						<h2>We are an award winning Agency</h2>
+						<div>take a look at our story</div>
 					</div>
 				</div>
 			</div>
-			<div class="row top_content">
+			<div class="row about_row">
+				<div class="col-lg-6 about_col order-lg-1 order-2">
+					<div class="about_content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla lectus nec diam auctor, ut fringilla diam sagittis. Quisque vel est id justo faucibus dapibus id a nibh. Aenean suscipit consequat lacus, sit amet mollis nulla. Morbi sagittis orci id lacus convallis tempus eget sit amet metus. Sed finibus, magna at euismod aliquet, enim justo vulputate lorem, sit amet elementum dolor eros sollicitudin dui. Sed ac magna mauris. Nullam lectus odio, viverra vel mi id, interdum imperdiet nulla. </p>
+						<div class="button about_button"><a href="#">Read More</a></div>
+					</div>
+				</div>
+				<div class="col-lg-6 about_col order-lg-2 order-1">
+					<div class="about_image">
+						<img src="frontend/images/about.jpg" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-				<div class="col-lg-3 col-md-6 top_col">
+	<!-- Milestones -->
 
-					<!-- Top Destination Item -->
-					<div class="top_item">
-						<a href="#">
-							<div class="top_item_image"><img src="frontend/images/top_1.jpg" alt="https://unsplash.com/@sgabriel"></div>
-							<div class="top_item_content">
-								<div class="top_item_price">From $890</div>
-								<div class="top_item_text">Paris, France</div>
-							</div>
-						</a>
+	<div class="milestones">
+		<div class="milestones_background parallax-window" data-parallax="scroll" data-image-src="frontend/images/fact_background.jpg" data-speed="0.8"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title text-center">
+						<h2>Fun facts about our Agency</h2>
+						<div>take a look</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2">
+					<div class="milestones_text">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla lectus nec diam auctor, ut fringilla diam sagittis. Quisque vel est id justo faucibus dapibus id a nibh</p>
+					</div>
+				</div>
+					
+			</div>
+			<div class="row milestones_container">
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="frontend/images/milestone_1.svg" alt=""></div>
+						<div class="milestone_counter" data-end-value="17">0</div>
+						<div class="milestone_text">Mountains Climbed</div>
 					</div>
 				</div>
 
-				<div class="col-lg-3 col-md-6 top_col">
-
-					<!-- Top Destination Item -->
-					<div class="top_item">
-						<a href="#">
-							<div class="top_item_image"><img src="frontend/images/top_2.jpg" alt="https://unsplash.com/@jenspeter"></div>
-							<div class="top_item_content">
-								<div class="top_item_price">From $890</div>
-								<div class="top_item_text">Italian Riviera</div>
-							</div>
-						</a>
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="frontend/images/milestone_2.svg" alt=""></div>
+						<div class="milestone_counter" data-end-value="213">0</div>
+						<div class="milestone_text">Islands Visited</div>
 					</div>
 				</div>
 
-				<div class="col-lg-3 col-md-6 top_col">
-
-					<!-- Top Destination Item -->
-					<div class="top_item">
-						<a href="#">
-							<div class="top_item_image"><img src="frontend/images/top_3.jpg" alt="https://unsplash.com/@anikindimitry"></div>
-							<div class="top_item_content">
-								<div class="top_item_price">From $890</div>
-								<div class="top_item_text">Cinque Terre</div>
-							</div>
-						</a>
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="frontend/images/milestone_3.svg" alt=""></div>
+						<div class="milestone_counter" data-end-value="11923">0</div>
+						<div class="milestone_text">Photos Taken</div>
 					</div>
 				</div>
 
-				<div class="col-lg-3 col-md-6 top_col">
-
-					<!-- Top Destination Item -->
-					<div class="top_item">
-						<a href="#">
-							<div class="top_item_image"><img src="frontend/images/top_4.jpg" alt="https://unsplash.com/@hellolightbulb"></div>
-							<div class="top_item_content">
-								<div class="top_item_price">From $890</div>
-								<div class="top_item_text">Santorini, Greece</div>
-							</div>
-						</a>
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="frontend/images/milestone_4.svg" alt=""></div>
+						<div class="milestone_counter" data-end-value="15">0</div>
+						<div class="milestone_text">Cruises Organized</div>
 					</div>
 				</div>
 
@@ -242,15 +272,53 @@
 		</div>
 	</div>
 
-	<!-- Last -->
+	<!-- Services -->
 
+	<div class="services">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title text-center">
+						<h2>Popular services that we offer</h2>
+						<div>take a look at these offers</div>
+					</div>
+				</div>
+			</div>
+			<div class="row icon_box_container">
 
+				<!-- Icon Box -->
+				<div class="col-lg-4 icon_box_col">
+					<div class="icon_box">
+						<div class="icon_box_image"><img src="frontend/images/service_1.svg" class="svg" alt="https://www.flaticon.com/authors/monkik"></div>
+						<div class="icon_box_title">Weekend trips</div>
+						<p class="icon_box_text">Lorem ipsum dolor sit amet, consectetur adip iscing elit. Fusce fringilla lectus nec diam auctor, ut fringilla diam sagittis.</p>
+						<a href="#" class="icon_box_more">Read More</a>
+					</div>
+				</div>
 
-	
+				<!-- Icon Box -->
+				<div class="col-lg-4 icon_box_col">
+					<div class="icon_box">
+						<div class="icon_box_image"><img src="frontend/images/service_2.svg" class="svg" alt="https://www.flaticon.com/authors/monkik"></div>
+						<div class="icon_box_title">Fun leisure trips</div>
+						<p class="icon_box_text">Lorem ipsum dolor sit amet, consectetur adip iscing elit. Fusce fringilla lectus nec diam auctor, ut fringilla diam sagittis.</p>
+						<a href="#" class="icon_box_more">Read More</a>
+					</div>
+				</div>
 
-	
+				<!-- Icon Box -->
+				<div class="col-lg-4 icon_box_col">
+					<div class="icon_box">
+						<div class="icon_box_image"><img src="frontend/images/service_3.svg" class="svg" alt="https://www.flaticon.com/authors/monkik"></div>
+						<div class="icon_box_title">Plane tickets</div>
+						<p class="icon_box_text">Lorem ipsum dolor sit amet, consectetur adip iscing elit. Fusce fringilla lectus nec diam auctor, ut fringilla diam sagittis.</p>
+						<a href="#" class="icon_box_more">Read More</a>
+					</div>
+				</div>
 
-	
+			</div>
+		</div>
+	</div>
 
 	<!-- Newsletter -->
 
@@ -288,14 +356,14 @@
 						<!-- Logo -->
 						<div class="logo_container">
 							<div class="logo">
-								<div>Train Management</div>
-								<div>Travel</div>
+								<div>destino</div>
+								<div>travel agency</div>
 								<div class="logo_image"><img src="frontend/images/logo.png" alt=""></div>
 							</div>
 						</div>
-						<div class="footer_about_text">This is footer</div>
+						<div class="footer_about_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar sed mauris eget tincidunt. Sed lectus nulla, tempor vel eleifend quis, tempus rut rum metus. Pellentesque ultricies enim eu quam fermentum hendrerit.</div>
 						<div class="copyright">
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> <i class="fa fa-heart-o" aria-hidden="true"></i> 
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> <i class="fa fa-heart-o" aria-hidden="true"></i> <a href="https://www.templateshub.net" target="_blank">Templates Hub</a>
 </div>
 					</div>
 				</div>
@@ -306,10 +374,23 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> <i cl
 						<div class="footer_title">Latest News</div>
 						<div class="footer_latest_content">
 
-							<!-- Footer 2nd column -->
+							<!-- Footer Latest Post -->
+							<div class="footer_latest_item">
+								<div class="footer_latest_image"><img src="frontend/images/latest_1.jpg" alt=""></div>
+								<div class="footer_latest_item_content">
+									<div class="footer_latest_item_title"><a href="news.html">Brazil Summer</a></div>
+									<div class="footer_latest_item_date">Jan 09, 2018</div>
+								</div>
+							</div>
 
-							add here
-							
+							<!-- Footer Latest Post -->
+							<div class="footer_latest_item">
+								<div class="footer_latest_image"><img src="frontend/images/latest_2.jpg" alt=""></div>
+								<div class="footer_latest_item_content">
+									<div class="footer_latest_item_title"><a href="news.html">A perfect vacation</a></div>
+									<div class="footer_latest_item_date">Jan 09, 2018</div>
+								</div>
+							</div>
 
 						</div>
 					</div>
@@ -319,7 +400,17 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> <i cl
 				<div class="col-lg-4 footer_col">
 					<div class="tags footer_tags">
 						<div class="footer_title">Tags</div>
-						footer 4th column
+						<ul class="tags_content d-flex flex-row flex-wrap align-items-start justify-content-start">
+							<li class="tag"><a href="#">travel</a></li>
+							<li class="tag"><a href="#">summer</a></li>
+							<li class="tag"><a href="#">cruise</a></li>
+							<li class="tag"><a href="#">beach</a></li>
+							<li class="tag"><a href="#">offer</a></li>
+							<li class="tag"><a href="#">vacation</a></li>
+							<li class="tag"><a href="#">trip</a></li>
+							<li class="tag"><a href="#">city break</a></li>
+							<li class="tag"><a href="#">adventure</a></li>
+						</ul>
 					</div>
 				</div>
 
@@ -331,10 +422,13 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> <i cl
 <script src="frontend/js/jquery-3.2.1.min.js"></script>
 <script src="frontend/styles/bootstrap4/popper.js"></script>
 <script src="frontend/styles/bootstrap4/bootstrap.min.js"></script>
-<script src="frontend/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="frontend/plugins/greensock/TweenMax.min.js"></script>
+<script src="frontend/plugins/greensock/TimelineMax.min.js"></script>
+<script src="frontend/plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="frontend/plugins/greensock/animation.gsap.min.js"></script>
+<script src="frontend/plugins/greensock/ScrollToPlugin.min.js"></script>
 <script src="frontend/plugins/easing/easing.js"></script>
 <script src="frontend/plugins/parallax-js-master/parallax.min.js"></script>
-<script src="frontend/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
-<script src="frontend/js/custom.js"></script>
+<script src="frontend/js/about_custom.js"></script>
 </body>
 </html>
