4c62dc2c9bf52eafba262c65cf848088d4832ef3
PercyGFX <isurangabtk@gmail.com>
2023-07-28 22:18:57 +0530

    admin layout clean

--- train-ticket/resources/views/admin/layout.blade.php
+++ train-ticket/resources/views/admin/layout.blade.php
@@ -66,7 +66,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ URL::to('/admin') }}" class="brand-link">
       <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">E-Train Admin</span>
     </a>
@@ -79,7 +79,7 @@
           <img src="{{ asset('backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{ auth()->user()->fname }} {{ auth()->user()->lname }}</a>
+          <a href="{{ URL::to('/admin') }}" class="d-block">{{ auth()->user()->fname }} {{ auth()->user()->lname }}</a>
         </div>
       </div>
 
