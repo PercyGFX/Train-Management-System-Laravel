eeb75674063deb08678918635e53654754842f64
PercyGFX <isurangabtk@gmail.com>
2023-07-28 21:11:57 +0530

    pdf reprt

--- train-ticket/resources/views/admin/loyalitydiscount.blade.php
+++ train-ticket/resources/views/admin/loyalitydiscount.blade.php
@@ -38,7 +38,7 @@
               </div>
               <!-- /.card-header -->
               <div class="card-body">
-                <table id="example2" class="table table-bordered table-hover">
+                <table id="example1" class="table table-bordered table-hover">
                   <thead>
                   <tr>
                     
